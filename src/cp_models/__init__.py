2dac81f52dc9f2d081ae382265a6f8ff48535705
Brian <bfuentes@fi.uba.ar>
2026-03-23 23:11:27 -0300

    soluciono importacion circular, comento las importaciones enteras.

--- src/cp_models/__init__.py
+++ src/cp_models/__init__.py
@@ -1,14 +1,14 @@
 from .classification.split_conformal import SplitConformalClassifier
 from .regression.split_conformal_regressor import SplitConformalRegressor
 from .scores import AbsoluteScore, SquaredScore
-from . import models
-from . import utils
+#from . import models
+#from . import utils
 
 __all__ = [
     "SplitConformalClassifier",
     "SplitConformalRegressor", 
     "AbsoluteScore",
     "SquaredScore",
-    "models",
-    "utils"
+    #"models",
+    #"utils"
 ]
